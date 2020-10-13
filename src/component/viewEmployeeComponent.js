9026234656b2e2f87a4faae89974602cd69bc7cf
imransuet <imransuet@gmail.com>
2020-10-13 15:41:52 +0200

    improved view functionality

--- src/component/viewEmployeeComponent.js
+++ src/component/viewEmployeeComponent.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import EmployeeService from '../services/EmployeeService';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+import 'font-awesome/css/font-awesome.min.css';
+
 
 class viewEmployeeComponent extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-                    id:this.props.match.params.id,
-                    employee:{}
+        this.state = {
+            id: this.props.match.params.id,
+            employee: {}
 
         }
-        
+
     }
-    componentDidMount()
-    {
-        EmployeeService.getEmployeeById(this.state.id).then(res=>{
+    componentDidMount() {
+        EmployeeService.getEmployeeById(this.state.id).then(res => {
 
             this.setState({
-                employee:res.data
+                employee: res.data
             });
         });
 
@@ -25,24 +26,29 @@ class viewEmployeeComponent extends Component {
     render() {
         return (
             <div>
-               <div className="card col-md-6 offset-md-3 bg-info">
-                   <h3 className="text-center">Employee Deatails</h3>
-                   <div className="card-body">
-                       <div className="row">
-                      <label>First Name :</label>
-                  <div>{this.state.employee.firstName}</div>
-                       </div>
-                       <div className="row">
-                      <label>Last Name:</label>
-                  <div>{this.state.employee.lastName}</div>
-                       </div>
-                       <div className="row">
-                      <label>Email:</label>
-                  <div>{this.state.employee.emailId}</div>
-                       </div>
-                   </div>
-               </div>
-            </div>
+
+                
+                        
+               <div className="card">
+                         
+                         <div>
+                         <img src="../img.jpg" height="180" width="200"/>
+                         </div>
+                 
+                        <h3>{this.state.employee.firstName} {this.state.employee.lastName}</h3>
+                        <div>{this.state.employee.emailId}</div>
+                        <p>University of Pennsylvania </p>
+                        <div>
+                        <a href="#" ><i className="fa fa-dribbble"></i></a>
+                        <a href="#" style={{marginLeft:"10px"}}><i className="fa fa-twitter"></i></a>
+                        <a href="#" style={{marginLeft:"10px"}}><i className="fa fa-linkedin"></i></a>
+                        <a href="#" style={{marginLeft:"10px"}}><i className="fa fa-facebook"></i></a>
+                        </div>
+                        
+                    
+                </div>
+
+                </div>
         );
     }
 }
